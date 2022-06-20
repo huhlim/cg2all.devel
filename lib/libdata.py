added data normalization; fixed HSD issue

lib/libdata.py
@@ -17,6 +17,21 @@ import libcg
 from libconfig import BASE, DTYPE
 from residue_constants import AMINO_ACID_s, residue_s
 
+
+class Normalizer(object):
+    def __init__(self, mean, std, n_scalar=38):
+        self.mean = mean
+        self.std = std
+        #
+        # apply only on scalar data
+        self.mean[n_scalar:] = 0.0
+        self.std[n_scalar:] = 1.0
+        self.std[self.std==0.0] = 1.0
+
+    def __call__(self, X):
+        return (X-self.mean) / self.std
+
+
 # %%
 class PDBset(torch_geometric.data.Dataset):
     def __init__(
@@ -26,6 +41,7 @@ class PDBset(torch_geometric.data.Dataset):
         cg_model,
         noise_level=0.0,
         get_structure_information=False,
+        cached=False,
     ):
         super().__init__()
         #
@@ -41,11 +57,25 @@ class PDBset(torch_geometric.data.Dataset):
         self.cg_model = cg_model
         self.noise_level = noise_level
         self.get_structure_information = get_structure_information
+        #
+        transform_npy_fn = basedir / "transform.npy"
+        if transform_npy_fn.exists():
+            self.transform = Normalizer(*np.load(transform_npy_fn))
+        else:
+            self.transform = None
+        #
+        self.cached = {}
+        if cached:
+            for i in range(len(self)):
+                self.cached[i] = self[i]
 
     def __len__(self):
         return self.n_pdb
 
     def __getitem__(self, index) -> torch_geometric.data.Data:
+        if index in self.cached:
+            return self.cached[index]
+        #
         pdb_id = self.pdb_s[index]
         pdb_fn = self.basedir / f"{pdb_id}.pdb"
         #
@@ -68,7 +98,7 @@ class PDBset(torch_geometric.data.Dataset):
         geom_s = cg.get_local_geometry(r)
         #
         data = torch_geometric.data.Data(
-            pos=torch.tensor(r[cg.atom_mask_cg == 1.0], dtype=DTYPE)
+            pos=torch.as_tensor(r[cg.atom_mask_cg == 1.0], dtype=DTYPE)
         )
         data.pos0 = data.pos.clone()
         #
@@ -84,8 +114,8 @@ class PDBset(torch_geometric.data.Dataset):
         f_in = [[], []]  # 0d, 1d
         # 0d
         # one-hot encoding of residue type
-        index = cg.bead_index[cg.atom_mask_cg == 1.0]
-        f_in[0].append(np.eye(cg.max_bead_type)[index])  # 22
+        cg_index = cg.bead_index[cg.atom_mask_cg == 1.0]
+        f_in[0].append(np.eye(cg.max_bead_type)[cg_index])  # 22
         f_in[0].append(n_neigh)  # 1
         #
         f_in[0].append(geom_s["bond_length"][1][0][:, None])  # 4
@@ -101,7 +131,7 @@ class PDBset(torch_geometric.data.Dataset):
         # noise-level
         f_in[0].append(np.full((r.shape[0], 1), noise_size))  # 1
         #
-        f_in[0] = torch.tensor(
+        f_in[0] = torch.as_tensor(
             np.concatenate(f_in[0], axis=1), dtype=DTYPE
         )  # 38x0e = 38
         #
@@ -110,7 +140,7 @@ class PDBset(torch_geometric.data.Dataset):
         f_in[1].append(geom_s["bond_vector"][1][1])
         f_in[1].append(geom_s["bond_vector"][2][0])
         f_in[1].append(geom_s["bond_vector"][2][1])
-        f_in[1] = torch.tensor(
+        f_in[1] = torch.as_tensor(
             np.concatenate(f_in[1], axis=1), dtype=DTYPE
         )  # 4x1o = 12
         f_in = torch.cat(
@@ -120,20 +150,24 @@ class PDBset(torch_geometric.data.Dataset):
             ],
             dim=1,
         )  # 38x0e + 12x1o = 50
-        data.f_in = f_in
+        if self.transform:
+            data.f_in = self.transform(f_in)
+        else:
+            data.f_in = f_in
         #
-        data.chain_index = torch.tensor(cg.chain_index, dtype=int)
-        data.residue_type = torch.tensor(cg.residue_index, dtype=int)
-        data.continuous = torch.tensor(cg.continuous, dtype=DTYPE)
-        data.output_atom_mask = torch.tensor(cg.atom_mask, dtype=DTYPE)
-        data.output_xyz = torch.tensor(cg.R[frame_index], dtype=DTYPE)
+        data.chain_index = torch.as_tensor(cg.chain_index, dtype=int)
+        data.residue_type = torch.as_tensor(cg.residue_index, dtype=int)
+        data.continuous = torch.as_tensor(cg.continuous, dtype=DTYPE)
+        data.output_atom_mask = torch.as_tensor(cg.atom_mask, dtype=DTYPE)
+        data.output_xyz = torch.as_tensor(cg.R[frame_index], dtype=DTYPE)
         #
         if self.get_structure_information:
             cg.get_structure_information()
-            data.correct_bb = torch.tensor(cg.bb[frame_index], dtype=DTYPE)
+            data.correct_bb = torch.as_tensor(cg.bb[frame_index], dtype=DTYPE)
             data.correct_quat = e3nn.o3.matrix_to_quaternion(data.correct_bb[:, :3])
-            data.correct_torsion = torch.tensor(cg.torsion[frame_index], dtype=DTYPE)
-            data.torsion_mask = torch.tensor(cg.torsion_mask, dtype=DTYPE)
+            data.correct_torsion = torch.as_tensor(cg.torsion[frame_index], dtype=DTYPE)
+            data.torsion_mask = torch.as_tensor(cg.torsion_mask, dtype=DTYPE)
+        #
         return data
 
 
@@ -211,7 +245,7 @@ def test():
     cg_model = functools.partial(libcg.ResidueBasedModel, center_of_mass=True)
     #
     train_set = PDBset(
-        base_dir, pdblist, cg_model, noise_level=0.5, get_structure_information=True
+        base_dir, pdblist, cg_model, noise_level=0.5, get_structure_information=True, cached=True,
     )
     train_loader = torch_geometric.loader.DataLoader(
         train_set, batch_size=5, shuffle=True, num_workers=1

script/test_pl.py
@@ -27,21 +27,21 @@ class Model(pl.LightningModule):
     def forward(self, batch: torch_geometric.data.Batch):
         return self.model.forward(batch)
 
+#    def configure_optimizers(self):
+#        optimizer = torch.optim.Adam(self.parameters(), lr=0.0001)
+#        return optimizer
+
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
-        return optimizer
-
-    # def configure_optimizers(self):
-    #     optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
-    #     lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
-    #         optimizer,
-    #         [
-    #             torch.optim.lr_scheduler.LinearLR(optimizer, 0.1, 1.0, 10),
-    #             torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98),
-    #         ],
-    #         [10],  # milestone
-    #     )
-    #     return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
+        lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
+            optimizer,
+            [
+                torch.optim.lr_scheduler.LinearLR(optimizer, 0.1, 1.0, 10),
+                torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98),
+            ],
+            [10],  # milestone
+        )
+        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
 
     def get_loss_sum(self, loss):
         loss_sum = 0.0
@@ -141,57 +141,57 @@ class Model(pl.LightningModule):
 
 def main():
     hostname = os.getenv("HOSTNAME", "local")
-    if (hostname == "markov.bch.msu.edu" or hostname.startswith("gpu")) and True:
+    if (hostname == "markov.bch.msu.edu" or hostname.startswith("gpu")) and False:
         base_dir = pathlib.Path("./")
         pdb_dir = base_dir / "pdb.pisces"
         pdblist_train = pdb_dir / "targets.train"
         pdblist_test = pdb_dir / "targets.test"
         pdblist_val = pdb_dir / "targets.valid"
+        cached = False
+        pin_memory = False
     else:
         base_dir = pathlib.Path("./")
         pdb_dir = base_dir / "pdb.processed"
         pdblist_train = pdb_dir / "pdblist"
         pdblist_test = pdb_dir / "pdblist"
         pdblist_val = pdb_dir / "pdblist"
+        cached = True
+        pin_memory = True
     #
     cg_model = functools.partial(ResidueBasedModel, center_of_mass=True)
     _PDBset = functools.partial(
-        PDBset, cg_model=cg_model, noise_level=0.0, get_structure_information=True
+        PDBset, cg_model=cg_model, noise_level=0.0, get_structure_information=True, cached=cached,
     )
     #
     batch_size = 16
     train_set = _PDBset(pdb_dir, pdblist_train)
     train_loader = torch_geometric.loader.DataLoader(
-        train_set, batch_size=batch_size, shuffle=True, num_workers=8
+        train_set, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=pin_memory,
     )
     val_set = _PDBset(pdb_dir, pdblist_val)
     val_loader = torch_geometric.loader.DataLoader(
-        val_set, batch_size=batch_size, shuffle=False, num_workers=8
+        val_set, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=pin_memory,
     )
     test_set = _PDBset(pdb_dir, pdblist_test)
     test_loader = torch_geometric.loader.DataLoader(
-        test_set, batch_size=batch_size, shuffle=False, num_workers=8
+        test_set, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=pin_memory,
     )
     #
     config = copy.deepcopy(libmodel.CONFIG)
     config.update_from_flattened_dict(
         {
+            "globals.num_recycle": 2,
             "globals.loss_weight.rigid_body": 1.0,
-            "globals.loss_weight.FAPE_CA": 1.0,
-            # "globals.loss_weight.quaternion": 1.0,
-            # "globals.loss_weight.distance_matrix": 1.0,
-            # "globals.loss_weight.bonded_energy": 1.0,
-            # "globals.loss_weight.torsion_angle": 1.0,
+            "globals.loss_weight.FAPE_CA": 5.0,
+            "globals.loss_weight.bonded_energy": 1.0,
+            "globals.loss_weight.distance_matrix": 100.0,
+            "backbone.loss_weight.rigid_body": 0.5,
+            "backbone.loss_weight.FAPE_CA": 2.5,
+            "backbone.loss_weight.bonded_energy": 0.5,
+            "backbone.loss_weight.distance_matrix": 50.0,
+            #"globals.loss_weight.torsion_angle": 1.0,
         }
     )
-    # config.update_from_flattened_dict(
-    #     {
-    #         "feature_extraction.num_layers": 4,
-    #         "feature_extraction.mid_Irreps": "80x0e + 40x1o",
-    #         "backbone.num_layers": 4,
-    #         "backbone.mid_Irreps": "40x0e + 20x1o",
-    #     }
-    # )
     model = Model(config, compute_loss=True)
     trainer = pl.Trainer(
         max_epochs=100,
@@ -200,7 +200,7 @@ def main():
         check_val_every_n_epoch=5,
     )
     trainer.fit(model, train_loader, val_loader)
-    #trainer.test(model, test_loader)
+    trainer.test(model, test_loader)
 
 
 if __name__ == "__main__":